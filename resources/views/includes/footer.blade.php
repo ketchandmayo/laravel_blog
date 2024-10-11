a9f502d90cc64baa4869cf3ac1e7a7ee95242c08
denis <toxinden@gmail.com>
2024-10-12 03:35:59 +0500

    Small footer fix

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-light text-secondary text-end py-3 fixed-bottom border-top">
     <div class="me-3">
-        © {{config('app.name')}} {{$date}}
+        ©{{config('app.name')}} {{$date}}
     </div>
 </footer>
