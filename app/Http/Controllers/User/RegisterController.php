76d4396f906ff1b659cedbaf79fe93b9ca546ffd
denis <toxinden@gmail.com>
2024-10-22 22:02:02 +0500

    DB issues fixes Permission fixes

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Currency;
+use App\Models\User;
 use App\Rules\Phone;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
@@ -11,21 +13,28 @@ class RegisterController extends Controller
 {
     public function index()
     {
+        dd(Currency::first());
         return view('register.index');
     }
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             "name" => ['required', 'string', 'min:3', 'max:255'],
-            "email" => ['required', 'string', 'email', 'max:255'],
-            "phone" => ['required', 'string', new Phone],
-            "password" => ['required', 'string', 'confirmed', Password::min(8)->max(255)->letters()->numbers()->mixedCase()],
+            "email" => ['required', 'string', 'email', 'max:255'], /* , 'unique:users' */
+//            "phone" => ['required', 'string', new Phone],
+            "password" => ['required', 'string', 'confirmed', Password::min(8)->max(255)], /*->letters()->numbers()->mixedCase()*/
             "password_confirmation" => ['required', 'string', 'min:8', 'max:255'],
             "agree" => ['accepted'],
         ]);
 
-        dump($request->all());
+        $user = new User();
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->password = $validated['password'];
+        $user->save();
+
+        dd($request->all());
 
         return redirect()->route('user.posts');
     }
